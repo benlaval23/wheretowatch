30fc9b1b00b1ec64be64af8c4a2bc7e5835badb2
Benjamin Laval <benlaval23@gmail.com>
2024-12-23 21:24:08 +0000

    url3

--- index.py
+++ index.py
@@ -1,32 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
 import requests
+import json
+from os import environ
 from dotenv import load_dotenv
 import os
 
+
 app = Flask(__name__)
 load_dotenv()
-
-app.config["SECRET_KEY"] = "my_secret"
-
-# DBsetup
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get(
-    "DATABASE_URL") or "sqlite:///mmyDB.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-
-class Searches(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    search = db.Column(db.String(100), index=True, unique=False)
 
 class Search(FlaskForm):
     name = StringField("Enter film name", render_kw={
                        "placeholder": "Search a film or series..."})
     submit = SubmitField("")
-#FORMsetup - end
+
+# APIcall
+url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
+
+querystring = {"term": None, "country": "uk"}
+
+headers = {
+    'x-rapidapi-key': os.environ['API_KEY_'],
+    'x-rapidapi-host': "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+results_dict = json.loads(response.text)
+list_of_popular_results = []
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -46,9 +49,3 @@ def home():
         results_count = len(results_dict['results'])
         return render_template('index_search.html', template_results=results_dict, template_form=my_form, search=search, results_count=results_count)
     return render_template('index.html', template_form=my_form)
-
-# for result in results_dict['results']:
-#  print(result['name'])
-#  for location in result['locations']:
-#    print(location['display_name'])
-#    print(location['url'])
